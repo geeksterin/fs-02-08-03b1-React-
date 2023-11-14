484245e3dfb90c7cfabbdd66450261dd54b8455e
Samiul Khan <toughlifesamiul@gmail.com>
2023-11-14 21:00:19 +0530

    CRA-react-router-dom-useparams

--- CRA/reactstart/src/Pages/User.js
+++ CRA/reactstart/src/Pages/User.js
@@ -1,9 +1,24 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { getUser } from "../http/http.service";
 
 function User() {
+  const { userId } = useParams();
+  const [user, setUser] = useState({});
+  useEffect(() => {
+    async function get(id) {
+      const { data } = await getUser(id);
+      setUser(data);
+    }
+    get(userId);
+  }, []);
+
   return (
-    <div>User</div>
-  )
+    <div>
+      <h1>{user.login}</h1>
+      <p>{user.name}</p>
+    </div>
+  );
 }
 
-export default User
+export default User;
