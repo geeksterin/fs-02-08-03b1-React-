f60e56d3c50088f4c0ba92c43f4e096903c91d3f
Samiul Khan <toughlifesamiul@gmail.com>
2023-11-21 21:38:15 +0530

    firebase login complete

--- firebaseauth/src/Compoent/Login.js
+++ firebaseauth/src/Compoent/Login.js
@@ -1,21 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import { auth, provider } from "../firebase.config.js";
 import { signInWithPopup } from "firebase/auth";
 
 function Login() {
-  function handleClick() {
-    signInWithPopup(auth, provider)
-      .then((data) => {
-        console.log(data);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
+  const [user, setUser] = useState(null);
+  async function handleClick() {
+    try {
+      const { user } = await signInWithPopup(auth, provider);
+      setUser(user);
+      console.log(user);
+    } catch (error) {
+      console.error(error);
+    }
+    // signInWithPopup(auth, provider)
+    //   .then((data) => {
+    //     console.log(data);
+    //   })
+    //   .catch((err) => {
+    //     console.log(err);
+    //   });
   }
-  return (
-    <div>
-      <button onClick={handleClick}>SignIn</button>
-    </div>
+  return user ? (
+    <h1>{`weolcome ${user.displayName}`}</h1>
+  ) : (
+    <button onClick={handleClick}>SignIn</button>
   );
 }
 
