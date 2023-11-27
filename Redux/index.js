a8e020833cfe766513fa64d5da92a9a7e5ad60b2
Samiul Khan <toughlifesamiul@gmail.com>
2023-11-27 21:24:40 +0530

    Redux

--- Redux/index.js
+++ Redux/index.js
@@ -1,8 +1,12 @@
 const redux = require("redux");
 const createStore = redux.createStore;
 
+const intitialState = {
+  cakeCount: 10,
+};
+
 const BUY_CAKE = "BUY_CAKE"; //action String
-const BUY_ICECREAM = "BUY_ICECREAM";
+// const BUY_ICECREAM = "BUY_ICECREAM"; //action String
 
 function buyCake() {
   return {
@@ -10,12 +14,11 @@ function buyCake() {
   };
 } //action builder -> building BUY_CAKE action
 
-function buyIceCream() {
-  return {
-    type: BUY_ICECREAM,
-  };
-}
-
+// function buyIceCream() {
+//   return {
+//     type: BUY_ICECREAM,
+//   };
+// } //action builder -> building BUY_ICECREAM action
 /**
  dispatch(buyCake())
  dispatch("incrementCount")
@@ -24,3 +27,23 @@ function buyIceCream() {
  dispatch("incrementCunt")
  dispatch("incremetCount")
  */
+
+function reducer(state = intitialState, action) {
+  switch (action.type) {
+    case BUY_CAKE:
+      return {
+        ...state,
+        cakeCount: state.cakeCount - 1,
+      };
+  }
+}
+
+const store = createStore(reducer);
+
+// console.log(store.getState());
+store.dispatch(buyCake());
+store.dispatch(buyCake());
+store.dispatch(buyCake());
+store.dispatch(buyCake());
+
+console.log(store.getState());
