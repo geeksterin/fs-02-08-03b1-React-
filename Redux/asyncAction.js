b396f2d53dd35491cf64898df633a770da59737d
Samiul Khan <toughlifesamiul@gmail.com>
2023-11-28 20:48:32 +0530

    async

---
+++ Redux/asyncAction.js
@@ -0,0 +1,71 @@
+const redux = require("redux");
+const createStore = redux.createStore;
+const applyMiddleware = redux.applyMiddleware;
+const thunkMiddleware = require("redux-thunk").default;
+const axios = require("axios");
+
+//userState
+
+const userState = {
+  users: [],
+  loading: false,
+  error: "",
+};
+
+//action strings
+const FETCH_USERS_REQUEST = "FETCH_USERS_REQUEST";
+const FETCH_USERS_SUCCESS = "FETCH_USERS_SUCCESS";
+const FETCH_USERS_FAILURE = "FETCH_USERS_FAILURE";
+//action builders
+function fetchUsersRequest() {
+  return {
+    type: FETCH_USERS_REQUEST,
+  };
+}
+
+function fetchUsersSuccess(users) {
+  return {
+    type: FETCH_USERS_SUCCESS,
+    payload: users,
+  };
+}
+
+function fetchUsersFailure(error) {
+  return {
+    type: FETCH_USERS_FAILURE,
+    payload: error,
+  };
+}
+
+//user reducer
+const userReducer = (state = userState, action) => {
+  switch (action.type) {
+    case FETCH_USERS_REQUEST:
+      return {
+        ...state,
+        loading: true,
+      };
+
+    case FETCH_USERS_SUCCESS:
+      return {
+        ...state,
+        loading: false,
+        users: action.payload,
+        error: "",
+      };
+
+    case FETCH_USERS_FAILURE:
+      return {
+        ...state,
+        loading: false,
+        users: [],
+        error: action.payload,
+      };
+
+    default:
+      return state;
+  }
+};
+
+//store
+const store = createStore(userReducer, applyMiddleware(thunkMiddleware));
